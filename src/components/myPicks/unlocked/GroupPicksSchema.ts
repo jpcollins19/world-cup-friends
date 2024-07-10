295528b5015b6e25e6663c144a5e8347c2fccddc
josephcollins-toast <joseph.collins@toasttab.com>
2024-07-10 15:27:54 -0500

    test commit

--- src/components/myPicks/unlocked/GroupPicksSchema.ts
+++ src/components/myPicks/unlocked/GroupPicksSchema.ts
@@ -14,6 +14,8 @@ export type UserGroupPicksSchema = {
   tiebreaker: number;
 };
 
+const byah = 6;
+
 export const useSignInSchema = () => {
   return Yup.object().shape({
     email: Yup.string().min(1).required(),
